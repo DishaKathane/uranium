a6359d311921aaf7bb0c095bb181a7a00484027f
dishakathane <dishakathane1995@gmail.com>
2022-04-22 01:25:56 +0530

    21st april assignment on authorization

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,20 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+
+//====================================que1================================================
+
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  // console.log.(req.newAtribute);
+  res.send({ msg: savedData });
 };
+
+//====================================que2=================================================
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
@@ -39,26 +44,9 @@ const loginUser = async function (req, res) {
   res.setHeader("x-auth-token", token);
   res.send({ status: true, data: token });
 };
-
+//======================================que3=========================================================
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-  
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-thorium");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
-  let userId = req.params.userId;
+ let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
@@ -66,11 +54,18 @@ const getUserData = async function (req, res) {
   res.send({ status: true, data: userDetails });
 };
 
+
+  
+  // If a token is present then decode the token with verify function
+  // verify takes two inputs:
+  // Input 1 is the token to be decoded
+  // Input 2 is the same secret with which the token was generated
+  // Check the value of the decoded token yourself
+ 
+//======================================que4 =======================================================
+
 const updateUser = async function (req, res) {
-// Do the same steps here:
-// Check if the token is present
-// Check if the token present is a valid token
-// Return a different error message in both these cases
+
 
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
@@ -80,10 +75,28 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new:true});
+  res.send({ status: true, data: updatedUser });
 };
 
+//========================================que5==================================================
+
+const deleteUser= async function(req,res) {
+
+  
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    //Return an error if no user with the given id exists in the db
+    if (!user) {
+      return res.send("No such user exists");
+    }
+    
+    let deletedUser = await userModel.findOneAndUpdate({ _id: userId }, {$set:{isDeleted:true}}, {new:true});
+
+    res.send({ status: true, data: deletedUser });
+}
+
+module.exports.deleteUser = deleteUser;
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,8 +5,7 @@ const userSchema = new mongoose.Schema( {
     lastName: String,
     mobile: {
         type: String,
-
-        required: true
+      required: true
     },
     emailId: String,
     password: String,
@@ -14,6 +13,10 @@ const userSchema = new mongoose.Schema( {
         type: String,
         enum: ["male", "female", "other"]
     },
+    isDeleted: {
+        type:Boolean,
+        default:false
+    },  
     age: Number,
 }, { timestamps: true });
 
