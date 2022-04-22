6e554fa325ea23042bbf6bad15ca014caf0b1748
dishakathane <dishakathane1995@gmail.com>
2022-04-23 00:49:02 +0530

    22april auth-2 for inderstand

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://disha123:hl6LMcJIED1eCZhr@cluster0.hrerz.mongodb.net/shopping-site", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,11 @@
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
+    let token = req.headers["x-auth-token"]
+    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
 
     next()
 }
